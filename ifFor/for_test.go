43f7cffd3d6b79a35c55dec5af68bdc4571a0b3c
matongde <Admin@1234>
2020-11-18 20:03:53 +0800

    11

--- ifFor/for_test.go
+++ ifFor/for_test.go
@@ -3,11 +3,16 @@ package ifFor
 import "testing"
 
 func TestLoop(t *testing.T) {
-	// 循环操作  for循环  没while
+	// 循环操作  for循环  没while  只能通过 无限for循环做处理  参见while 文件夹
 
 	for i := 0; i <= 10; i++ {
 		t.Log(i)
 	}
+
+	//无限循环
+
+	//for {
+	//}
 
 	//a := 0
 	//while()s
